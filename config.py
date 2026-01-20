61824f21cf93546a4a8f83d2635ff8ab5cd91826
누룽지_집컴 <paulyoo999@gmail.com>
2026-01-20 23:01:18 +0900

    NTS 팀 추가

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ KAKAO_REST_KEY = "9d17b379d6a4de94c06563a990609336"
 KAKAO_JS_KEY = "69f821ba943db5e3532ac90ea5ca1080" 
 
 # 모드 설정
-IS_TEST_MODE = False
+IS_TEST_MODE = True
 JSON_FILE_NAME = "volleyball_clubs_kakao.json"
 MANIFEST_FILE_NAME = "manifest.json"
